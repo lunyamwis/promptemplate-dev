9599d48fe8ac54b8de16c962a7c498a3a9982c60
tutorialcreation <tutorialcreation81@gmail.com>
2023-12-04 10:18:21 +0300

    adding prompttemplate

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# source /root/.local/share/virtualenvs/brooks-insurance-*/bin/activate
+
+echo "<<<<<<<< Collect Staticfiles>>>>>>>>>"
+python3 manage.py collectstatic --noinput
+
+
+sleep 5
+echo "<<<<<<<< Database Setup and Migrations Starts >>>>>>>>>"
+# Run database migrations
+# python3 manage.py makemigrations
+python3 manage.py migrate &
+
+sleep 5
+echo "<<<<<<<<<<<<<<<<<<<< START API >>>>>>>>>>>>>>>>>>>>>>>>"
+python manage.py runserver 0.0.0.0:8000
+# Start the API with gunicorn
+# gunicorn --bind 0.0.0.0:8000 setup.wsgi --reload --access-logfile '-' --workers=2
