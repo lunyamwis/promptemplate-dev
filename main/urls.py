cbe871dfae1bdf454fc21d990f18871dfa323820
tutorialcreation <tutorialcreation81@gmail.com>
2023-12-15 12:46:05 +0300

    adding static files

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 admin.site.site_header = "Scripting Microservice Management"
@@ -23,5 +25,5 @@ admin.site.site_header = "Scripting Microservice Management"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('prompt.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
