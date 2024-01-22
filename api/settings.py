6492cfbf0d54dc67b342474dcfd2f405b3f5617d
tutorialcreation <tutorialcreation81@gmail.com>
2024-01-22 10:53:28 +0300

    added new developments

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-utr163745j!iq*)7h-+g6_!y+z$mkmcx3x2ouv$gq$8-42)yn+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["34.28.104.255","127.0.0.1"]
+ALLOWED_HOSTS = ["34.28.104.255","127.0.0.1","0.0.0.0"]
 CSRF_TRUSTED_ORIGINS = ["http://34.28.104.255"]
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.instagram',
     'rest_framework',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
