02d2f3d91e6e1b64769b31d31e523129606ddea2
tutorialcreation <tutorialcreation81@gmail.com>
2024-01-23 11:18:46 +0300

    enriching data alone

--- api/instagram/tasks.py
+++ api/instagram/tasks.py
@@ -6,14 +6,14 @@ from boostedchatScrapper.spiders.instagram_booksy import InstagramSpider
 def scrap_instagram(accounts,followers):
     inst = InstagramSpider()
     # inst.insert_data_with_enriched_outsourced_data(accounts,followers)
-    for account in accounts:
-        print(account.lower())
-        try:
-            users = inst.get_ig_user_info(account.lower(),followers)
-            try:
-                inst.enrich_outsourced_data(tuple(users[0]),infinite= True)
-            except Exception as error:
-                print(error)
-        except Exception as error:
-            print(error)
+    # for account in accounts:
+    #     print(account.lower())
+    #     try:
+    #         users = inst.get_ig_user_info(account.lower(),followers)
+    #         try:
+    #             inst.enrich_outsourced_data(tuple(users[0]),infinite= True)
+    #         except Exception as error:
+    #             print(error)
+    #     except Exception as error:
+    #         print(error)
     inst.enrich_outsourced_data()
