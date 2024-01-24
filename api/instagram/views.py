3d0c7a67235765f36b61de50d41f42ecaa5279e7
tutorialcreation <tutorialcreation81@gmail.com>
2024-01-24 11:19:08 +0300

    changing login credentials

--- api/instagram/views.py
+++ api/instagram/views.py
@@ -17,7 +17,7 @@ class InstagramScrapper(APIView):
 
 class InstagramMoxieCsv(APIView):
     def post(self, request):
-        client = login_user(username='stella.elth',password='martinnyambane1996-')
+        client = login_user(username='matisti96', password='luther1996-')
         _, cursor = client.user_followers_gql_chunk(client.user_id_from_username(request.data.get('username')), max_amount=100)
         process_followers_dry.delay(client,cursor,request.data.get('username'))
         return Response({"success":True},status=status.HTTP_200_OK)
